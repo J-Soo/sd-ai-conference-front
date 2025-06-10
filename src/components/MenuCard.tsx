b935c67bb598dffd61a1390a3864618b1f4f7c3d
J-Soo <159222519+J-Soo@users.noreply.github.com>
2025-06-11 01:52:38 +0900

    Updated MenuCard.tsx

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -63,7 +63,7 @@ const MenuCard: React.FC<MenuCardProps> = ({
     <div
       onClick={() => onNavigate(id)}
       className={`group cursor-pointer rounded-xl border-2 transition-all duration-300 transform hover:scale-105 hover:shadow-lg
-        w-96 h-[28rem] p-10 pb-16 flex flex-col
+        w-96 h-[28rem] p-10 pb-16 flex flex-col mb-12
         ${colors.bg} ${colors.border}`}
     >
       <div className="flex items-start justify-between mb-8">
